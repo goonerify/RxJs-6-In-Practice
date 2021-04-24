9bd469b307b3c8dcb93ac5e57f054f8e5231059b
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-24 01:29:17 -0400

    9. Learn How Observables Work Under the Hood, Build Your Own HTTP Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { noop, Observable } from 'rxjs';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    // const interval$ = interval(2000);
+    // const sub = interval$.subscribe((val) => console.log('stream 1 => ' + val));
+    // setTimeout(() => sub.unsubscribe(), 5000);
+    // interval$.subscribe((val) => console.log('stream 2 => ' + val));
+    // const timer$ = timer(3000, 1000);
+    // timer$.subscribe((val) => console.log(val));
+    // const click$ = fromEvent(document, 'click');
+    // click$.subscribe((evt) => console.dir(evt));
 
+    const http$ = new Observable((observer) => {
+      fetch('/api/courses')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          observer.next(data);
+          observer.complete();
+        })
+        .catch((err) => {
+          observer.error(err);
+        });
+    });
+
+    http$.subscribe({
+      next: (data) => console.dir(data),
+      error: noop,
+      complete: () => console.log('completed'),
+    });
+  }
 }
