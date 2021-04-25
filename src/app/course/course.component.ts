096223780e989aa0339d6293ffd10c76d0da5c37
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-25 04:00:48 -0400

    23. Building a Search Typeahead - debounceTime and distinctUntilChanged Operators

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -6,8 +6,9 @@ import {
   ViewChild,
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 import { map } from 'rxjs/internal/operators/map';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { createHttpObservable } from '../common/util';
 import { Course } from '../model/course';
 import { Lesson } from '../model/lesson';
@@ -33,5 +34,34 @@ export class CourseComponent implements OnInit, AfterViewInit {
     ).pipe(map((res) => res.payload));
   }
 
-  ngAfterViewInit() {}
+  ngAfterViewInit() {
+    fromEvent<any>(this.input.nativeElement, 'keyup')
+      .pipe(
+        map((event) => event.target.value),
+        // The DebounceTimeoperator enforces stability, similar to the ExhaustMap operator
+        // that is used with DOM elements.
+        // To use the DebounceTimeoperator, you would specify an interval and then any
+        // data entering the input stream during the interval must remain stable to be
+        // emitted by the observable. If new data enters the input stream then the previous data is ignored.
+
+        // At the end of the interval, only the most recent data in the stream is emitted
+        // by the observable
+
+        // This solves the problem of Double entry in a TypeAhead from holding down the
+        // shift key
+        debounceTime(400),
+        // The DistinctUntilChanged operator will emit only one value if 2 values in the input stream are exactly the same
+        distinctUntilChanged()
+        // concatMap((event) => this.filterCourse(event.target.value)),
+        // map((res) => (this.lessons$ = of(res.payload)))
+      )
+      .subscribe(console.log);
+  }
+
+  //   filterCourse(value) {
+  //     const courseId = this.route.snapshot.params['id'];
+  //     const url = `/api/lessons?courseId=${courseId}&filter=${value}`;
+
+  //     return createHttpObservable(url);
+  //   }
 }
