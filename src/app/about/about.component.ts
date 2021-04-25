22317c7b95d85dbda3861b28e22b558050956b5a
Leslie Enwerem <lenwerem@earthwarm.org>
2021-04-25 00:44:10 -0400

    18. Understanding the merge Observable combination Strategy

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { concat, of } from 'rxjs';
+import { interval, merge } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
 
 @Component({
   selector: 'about',
@@ -10,14 +11,9 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    const source1$ = of(1, 2, 3);
-    // In order to concatenate observables, each of the observables must complete
-    // or else values of subsequent observables will never be emitted
-    // const source1$ = interval(1000);
-    const source2$ = of(4, 5, 6);
-    const source3$ = of(7, 8, 9);
-
-    const result$ = concat(source1$, source2$, source3$);
+    const interval1$ = interval(1000);
+    const interval2$ = interval1$.pipe(map((val) => val * 10));
+    const result$ = merge(interval1$, interval2$);
     result$.subscribe(console.log);
   }
 }
